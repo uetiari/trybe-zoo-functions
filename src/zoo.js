44c8837acb1d787a302f226e8716839bd32c6e89
Ariane Ueti <arianeueti@gmail.com>
2021-08-07 17:34:28 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -38,8 +38,14 @@ function isManager(id) {
   return employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function countAnimals(specie) {
